e0e51595a4a7f7fa76c0e55d7d55be0e0896a137
Julião LB <104986669+JLB-101@users.noreply.github.com>
2023-08-04 17:54:05 +0200

    update app

---
+++ src/duke/choice/ShopApp.java
@@ -0,0 +1,7 @@
+package duke.choice;
+public class ShopApp {
+    public static void main(String[] args) {
+        //Main app
+        System.out.print("Welcome to Duke Choice Shop !!!");
+    }
+}
